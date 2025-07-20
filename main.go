c0233549ffd15059a2248a4015053f50c5bd807f
Ricky Hariady <ricky.hariady@tech.jago.com>
2025-07-20 15:50:05 +0700

    implement key legend as grid

--- main.go
+++ main.go
@@ -80,9 +80,24 @@ func main() {
  / /    \__ \/ / / / / /
 / /___ ___/ / /_/ / / /___
 \____//____/\___\_\/_____/`)
-		keyLegend := tview.NewTextView().SetText(`(q) quit
-(a) add instance
-(enter) select`)
+
+		keyBindings := []struct {
+			Key     string
+			Purpose string
+		}{
+			{"q", "quit"},
+			{"a", "add instance"},
+			{"enter", "select"},
+		}
+
+		keyLegend := tview.NewGrid().
+			SetRows(1,1,1,1,1,1)
+
+		for i, binding := range keyBindings {
+			x := i/6
+			y := i%6
+			keyLegend.AddItem(tview.NewTextView().SetText(fmt.Sprintf("<%s> %s", binding.Key, binding.Purpose)), y, x, 1, 1, 0, 0, false)
+		}
 
 		header := tview.NewFlex().
 			AddItem(keyLegend, 0, 1, false).
