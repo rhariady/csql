d966314b063c3723c22bde8e684330e286cb2985
Ricky Hariady <ricky.hariady@tech.jago.com>
2025-08-03 09:57:08 +0700

    add change database modal

---
+++ pkg/dbadapter/postgresql/change_database.go
@@ -0,0 +1,70 @@
+package postgresql
+
+import (
+	"fmt"
+	
+	"github.com/rivo/tview"
+
+	"github.com/rhariady/csql/pkg/session"
+)
+
+type ChangeDatabaseModal struct {
+	*PostgreSQLAdapter
+	
+	last_view session.View
+}
+
+func NewChangeDatabaseModal(adapter *PostgreSQLAdapter, last_view session.View) *ChangeDatabaseModal {
+	return &ChangeDatabaseModal{
+		PostgreSQLAdapter: adapter,
+		last_view: last_view,
+	}
+}
+
+func (d *ChangeDatabaseModal) GetTitle() string {
+	return "Select a database"
+}
+
+func (d *ChangeDatabaseModal) GetContent(session *session.Session) tview.Primitive {
+	databaseTable := tview.NewTable().
+		SetBorders(false).
+		SetSelectable(true, false)
+
+	databases, _ := d.PostgreSQLAdapter.listDatabases()
+
+	row := 0
+	for _, database := range databases {
+		databaseTable.SetCell(row, 0, tview.NewTableCell(database.Name).SetExpansion(1))
+		row++
+	}
+
+	databaseTable.Select(0, 0)
+
+	databaseTable.SetSelectedFunc(func(row int, column int) {
+		// databaseList := NewDatabaseList(i.instanceName, userName)
+		session.CloseModal()
+
+		newDatabase := databaseTable.GetCell(row, 0).Text
+
+		d.database = newDatabase
+		err := d.openConnection()
+
+		if err != nil {
+			session.ShowMessage(fmt.Sprintf("Error:\n%s", err), true)
+		}
+
+		tableList := NewTableList(d.PostgreSQLAdapter)
+		session.SetView(tableList)		
+	})
+
+	return databaseTable
+	
+}
+
+func (d *ChangeDatabaseModal) GetKeyBindings() (keybinding []*session.KeyBinding) {
+	return
+}
+
+func (d *ChangeDatabaseModal) GetInfo() (infe []session.Info) {
+	return
+}
